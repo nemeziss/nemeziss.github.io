8b2fcbe8d3f06766629e547322d305ad3bd5d4e3
nemeziss <30736642+nemeziss@users.noreply.github.com>
2017-08-05 12:32:24 +0300

    qyq
    qyqy

--- js/common.js
+++ js/common.js
@@ -1,13 +1,9 @@
-	var a = 0;
-	function clickbtnmenu(){
-					var givememenu = document.getElementById("mocha");
-					givememenu.style.display = "block";
-					a = 1;
+function clickbtnmenu() {
+	var hideshowlist = document.getElementById('mocha');
+  hideshowlist.style.display = (hideshowlist.style.display == 'none') ? 'block' : 'none';
 }
 
 function hidemenu() {
-if (a==1) {
 	var hidethis = document.getElementById("mocha");
 	hidethis.style.display = "none";
-}
 }
