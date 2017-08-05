5b7fcd1387278e186b16e24410d1f57868b20059
nemeziss <30736642+nemeziss@users.noreply.github.com>
2017-08-05 12:54:50 +0300

    jj
    yyy

--- js/common.js
+++ js/common.js
@@ -2,8 +2,3 @@ function clickbtnmenu() {
 	var hideshowlist = document.getElementById('mocha');
   hideshowlist.style.display = (hideshowlist.style.display == 'block') ? 'none' : 'block';
 }
-
-function hidemenu() {
-	var hidethis = document.getElementById("mocha");
-	hidethis.style.display = "none";
-}
